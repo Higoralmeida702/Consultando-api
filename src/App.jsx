7a7e5bc5ecd046dfaf10fc398f0d2b0d2487faa3
Higoralmeida702 <higoralmeida7022@gmail.com>
2024-07-02 17:29:28 -0300

    Implementa fetch de dados da API REST Countries e armazena em estado local
    - Adiciona uso de axios para realizar a requisição GET à API REST Countries.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
 function App() {
-  return <></>;
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = async () => {
+    try {
+      const response = await axios.get("https://restcountries.com/v3.1/all");
+      const data = response.data;
+      setPosts(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  return;
+  <div></div>;
 }
 
 export default App;
